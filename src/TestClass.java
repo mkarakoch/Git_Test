1096725484f445b7c54546fe31431333027a1376
mkarakoch <88216924+mkarakoch@users.noreply.github.com>
2021-07-30 22:31:18 -0400

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,7 +1,7 @@
 public class TestClass{
     public static void main(String[] args) {
         System.out.println("Hello from Git");
-         System.out.println("GitHub dan Hallo");
+        System.out.println("GitHub dan Hallo");
     }
 }
 
